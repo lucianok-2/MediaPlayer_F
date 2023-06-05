c1b71b7535b8e3a1cf8a13b1eac845bcbe9b3762
Kuadrao <lucianoonatevenegas@gmail.com>
2023-06-05 16:23:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.media.MediaPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    private MediaPlayer mediaPlayer;
+    private GestureDetector gestureDetector;
+    private TextView statusTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.dbz_cancerbero); // R.raw.audio es el archivo de sonido generado
+        gestureDetector = new GestureDetector(this, this);
+        statusTextView = findViewById(R.id.statusTextView);
+
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                statusTextView.setText("Reproduciendo");
+            }
+        });
+
+        Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+                statusTextView.setText("Detenido");
+            }
+        });
+
+        Button loopButton = findViewById(R.id.loopButton);
+        loopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+                statusTextView.setText("Reproduciendo en bucle");
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (Math.abs(velocityY) > Math.abs(velocityX)) {
+            if (e1.getY() > e2.getY()) {
+                statusTextView.setText("Desplazamiento vertical hacia arriba");
+            } else {
+                statusTextView.setText("Desplazamiento vertical hacia abajo");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+}
